a97f9bdfe032bc2f2e7b9ec56ec2d7cb6cb0891e
Mahahein <mahahein3@gmail.com>
2014-06-25 02:59:07 -0400

    Creando los comentarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140625065608) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user"
+    t.text     "message"
+    t.datetime "time_sent"
+    t.boolean  "isFixed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
